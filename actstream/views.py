b7db5800eda510ba2b59cc6da31fb67075c234b3
saumishr <saumishr@ubuntu.(none)>
2013-05-12 23:41:53 +0530

    reshare post implementation

--- actstream/views.py
+++ actstream/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import simplejson
 
 from actstream import actions, models
 from actstream.models import Follow
