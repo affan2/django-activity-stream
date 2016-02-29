8340ef49560a292b92a854ed4e1b57ce15d8f2a9
Ahmed Salman <ahmed.salman.noor@gmail.com>
2016-02-29 19:46:22 +0500

    verb translations added

---
+++ actstream/verb_translation.py
@@ -0,0 +1,12 @@
+from django.utils.translation import ugettext_lazy as _
+
+verb_translation_dic = {
+    'received a new badge': _('received a new badge'),
+    'viewed an event': _('viewed an event'),
+    'levelled up': _('levelled up'),
+    'viewed an article': _('viewed an article'),
+    'viewed a company': _('viewed a company'),
+    'a news article was posted': _('a news article was posted'),
+    'posted a news article': _('posted a news article'),
+}
+
